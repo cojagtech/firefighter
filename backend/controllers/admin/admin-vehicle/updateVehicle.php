632a03a0cf01b39001152dd79fa922aea52a4bb9
cojagtech <project@cojagtech.com>
2026-03-24 17:04:48 +0530

    station update

--- backend/controllers/admin/admin-vehicle/updateVehicle.php
+++ backend/controllers/admin/admin-vehicle/updateVehicle.php
@@ -14,7 +14,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 }
 
 require_once realpath(__DIR__ . "/../../../config/db.php");
-require "../../../helpers/logActivity.php";
+require_once realpath(__DIR__ . "/../../../helpers/logActivity.php");
 
 /* ---------- INPUT ---------- */
 $data = json_decode(file_get_contents("php://input"), true);
